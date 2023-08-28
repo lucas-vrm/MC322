ff158ccbdd70c56b14ca52e3d04a23d67163f95d
Brunn <Brunn@DESKTOP-74L17QK>
2023-08-27 23:57:16 -0300

    classe relatorio

--- Lab01/src/Relatorio.java
+++ Lab01/src/Relatorio.java
@@ -1,9 +1,38 @@
 
 public class Relatorio {
+	private int data;
+	private String autor;
+	private int numeroPaginas;
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/*Metodo construtor*/
+	Relatorio(int data, String autor, int numeroPaginas){
+		this.data = data;
+		this.autor = autor;
+		this.numeroPaginas = numeroPaginas;
 	}
-
+	/*Metodos getters e setters*/
+	public int getData() {
+		return data;
+	}
+	
+	public void setData(int data) {
+		this.data = data;
+	}
+	
+	public String getAutor() {
+		return autor;
+	}
+	
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public int getNumeroPaginas() {
+		return numeroPaginas;
+	}
+	public void setNumeroPaginas(int numeroPaginas) {
+		this.numeroPaginas = numeroPaginas;
+	}
+	public static void main(String args) {
+		Relatorio relatorio1 = new Relatorio(21082023, "Roberto", 4);
+		System.out.println(relatorio1);
 }
